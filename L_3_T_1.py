0ee1e41386b18aa25da48fafe282f2858ea8894e
hermaer <91397176+hermaer@users.noreply.github.com>
2024-02-05 22:12:02 +0700

    Add files via upload

---
+++ L_3_T_1.py
@@ -0,0 +1,8 @@
+#  user data entry 
+view_pet = input("Введите цвет и вид вашего питомца: ")
+age_pet = input("Введите возраст питомца: ")
+name_pet = input("Введите кличку питомца: ")
+#  user data output
+print(f"Ваш питомец это {view_pet} по кличке {name_pet}. Возраст: {age_pet} года ")
+
+
